0eb48520975e3021a1c0003decbdce25c65a8edd
Lyudmila <88749276+ldubrovina@users.noreply.github.com>
2022-01-23 02:23:34 +0300

    add scroll and tabs

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,28 +1,36 @@
-const links = document.querySelectorAll(".header-menu__item a");
-
-seamless.polyfill();
-
-links.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const id = element.getAttribute("href").substring(1);
-    const section = document.getElementById(id);
-
-    if (section) {
-      seamless.elementScrollIntoView(section, {
-        behavior: "smooth",
-        block: "start",
-      });
-    } else {
-      seamless.elementScrollIntoView(
-        document.querySelector("#characteristics"),
-        {
+const scrollFunc () => {
+  const links = document.querySelectorAll(".header-menu__item a");
+  const linkCharacteristics = document.querySelector(
+    ".card-details__link-characteristics"
+  );
+  
+  const newArray = [...links, linkCharacteristics];
+  seamless.polyfill();
+  
+  newArray.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+  
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+  
+      if (section) {
+        seamless.elementScrollIntoView(section, {
           behavior: "smooth",
-          block: "center",
-          inline: "center",
-        }
-      );
-    }
+          block: "start",
+        });
+      } else {
+        seamless.elementScrollIntoView(
+          document.querySelector("#characteristics"),
+          {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+          }
+        );
+      }
+    });
   });
-});
+  
+}
+scrollFunc()
